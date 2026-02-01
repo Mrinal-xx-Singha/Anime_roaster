0ae9f164c02bd0dd66c33961c3759df7fad3c2fe
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2026-02-01 17:45:40 +0530

    working on the card logic

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "../components/ui/button";
+import AnimeList from "../components/AnimeList";
+
+const page = () => {
+  return (
+    <div>
+      <AnimeList />
+    </div>
+  );
+};
+
+export default page;
